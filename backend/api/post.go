ec2c349e4d7aa68d0ad5be99d2657daaa38a7b31
Christian Raditya Restanto <resradit@gmail.com>
2022-06-11 14:40:58 +0700

    feat: add read update delete post

--- backend/api/post.go
+++ backend/api/post.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rg-km/final-project-engineering-6/repository"
 )
 
 type CreatePostRequest struct {
@@ -13,8 +17,28 @@ type CreatePostRequest struct {
 	Description string `json:"description"`
 }
 
-type CreatePostReponse struct {
+type UpdatePostRequest struct {
+	ID          int    `json:"id"`
+	CategoryID  int    `json:"category_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type DetailPostResponse struct {
+	ID          int    `json:"id"`
+	AuthorID    int    `json:"author_id"`
+	CategoryID  int    `json:"category_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+}
+
+type SuccessPostResponse struct {
 	Message string `json:"message"`
+}
+
+type ErrorPostResponse struct {
+	Message string `json:"error"`
 }
 
 func (api *API) createPost(ctx *gin.Context) {
@@ -23,34 +47,121 @@ func (api *API) createPost(ctx *gin.Context) {
 	)
 
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, CreatePostReponse{Message: "Invalid Request Body"})
+		ctx.JSON(http.StatusBadRequest, ErrorPostResponse{Message: "Invalid Request Body"})
 		return
 	}
 
 	if err := api.postRepo.InsertPost(req.AuthorID, req.CategoryID, req.Title, req.Description); err != nil {
-		ctx.JSON(http.StatusInternalServerError, CreatePostReponse{Message: "Internal Server Error"})
+		ctx.JSON(http.StatusInternalServerError, ErrorPostResponse{Message: "Internal Server Error"})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, CreatePostReponse{Message: "Post Created"})
+	ctx.JSON(http.StatusOK, SuccessPostResponse{Message: "Post Created"})
 }
 
 func (api *API) readPosts(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Get All Post",
-	})
+	posts, err := api.postRepo.FetchAllPost()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ErrorPostResponse{Message: "Internal Server Error"})
+		return
+	}
+
+	postsReponse := make([]DetailPostResponse, len(posts))
+
+	for i, post := range posts {
+		postsReponse[i] = DetailPostResponse{
+			ID:          post.ID,
+			AuthorID:    post.AuthorID,
+			CategoryID:  post.CategoryID,
+			Title:       post.Title,
+			Description: post.Description,
+			CreatedAt:   post.CreatedAt.Format(time.RFC3339),
+		}
+	}
+
+	ctx.JSON(http.StatusOK, postsReponse)
 }
 
 func (api *API) readPost(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Get Post",
+	var (
+		postID int
+		err    error
+	)
+
+	if postID, err = strconv.Atoi(ctx.Param("id")); err != nil {
+		ctx.JSON(http.StatusBadRequest, ErrorPostResponse{Message: "Invalid Post ID"})
+		return
+	}
+
+	post, err := api.postRepo.FetchPostByID(postID)
+
+	if err != nil {
+		if errors.Is(err, repository.ErrPostNotFound) {
+			ctx.JSON(http.StatusNotFound, ErrorPostResponse{Message: "Post Not Found"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, ErrorPostResponse{Message: "Internal Server Error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, DetailPostResponse{
+		ID:          post.ID,
+		AuthorID:    post.AuthorID,
+		CategoryID:  post.CategoryID,
+		Title:       post.Title,
+		Description: post.Description,
+		CreatedAt:   post.CreatedAt.Format(time.RFC3339),
 	})
 }
 
 func (api *API) updatePost(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Update Post",
-	})
+	var (
+		req = UpdatePostRequest{}
+	)
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, ErrorPostResponse{Message: "Invalid Request Body"})
+		return
+	}
+
+	if err := api.postRepo.UpdatePost(req.ID, req.CategoryID, req.Title, req.Description); err != nil {
+
+		if errors.Is(err, repository.ErrPostNotFound) {
+			ctx.JSON(http.StatusNotFound, ErrorPostResponse{Message: "Post Not Found"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, ErrorPostResponse{Message: "Internal Server Error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, SuccessPostResponse{Message: "Post Updated"})
+
 }
 
-func (api *API) deletePost(ctx *gin.Context) {}
+func (api *API) deletePost(ctx *gin.Context) {
+	var (
+		postID int
+		err    error
+	)
+
+	if postID, err = strconv.Atoi(ctx.Param("id")); err != nil {
+		ctx.JSON(http.StatusBadRequest, ErrorPostResponse{Message: "Invalid Post ID"})
+		return
+	}
+
+	if err := api.postRepo.DeletePostByID(postID); err != nil {
+
+		if errors.Is(err, repository.ErrPostNotFound) {
+			ctx.JSON(http.StatusNotFound, ErrorPostResponse{Message: "Post Not Found"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, ErrorPostResponse{Message: "Internal Server Error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, SuccessPostResponse{Message: "Post Deleted"})
+}
--- backend/repository/posts.go
+++ backend/repository/posts.go
@@ -2,14 +2,28 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Post struct {
+	ID          int       `db:"id"`
+	AuthorID    int       `db:"author_id"`
+	CategoryID  int       `db:"category_id"`
+	Title       string    `db:"title"`
+	Description string    `db:"desc"`
+	CreatedAt   time.Time `db:"created_at"`
+}
+
 type PostRepository struct {
 	db *sql.DB
 }
+
+var (
+	ErrPostNotFound = errors.New("Post not found")
+)
 
 func NewPostRepository(db *sql.DB) *PostRepository {
 	return &PostRepository{
@@ -26,4 +40,95 @@ func (p *PostRepository) InsertPost(authorID, categoryID int, title, description
 	_, err := p.db.Exec(sqlStatement, authorID, categoryID, title, description, time.Now())
 
 	return err
+}
+
+func (p *PostRepository) FetchAllPost() ([]Post, error) {
+	sqlStatement := `SELECT * FROM posts ORDER BY created_at DESC;`
+
+	rows, err := p.db.Query(sqlStatement)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		err := rows.Scan(&post.ID, &post.AuthorID, &post.CategoryID, &post.Title, &post.Description, &post.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func (p *PostRepository) FetchPostByID(postID int) (Post, error) {
+	var (
+		post         Post
+		sqlStatement string
+	)
+
+	sqlStatement = `
+		SELECT * FROM posts WHERE id = ?;
+	`
+
+	row := p.db.QueryRow(sqlStatement, postID)
+
+	err := row.Scan(&post.ID, &post.AuthorID, &post.CategoryID, &post.Title, &post.Description, &post.CreatedAt)
+
+	if err == sql.ErrNoRows {
+		return post, ErrPostNotFound
+	}
+
+	return post, err
+}
+
+func (p *PostRepository) UpdatePost(postID, categoryID int, title, description string) error {
+	sqlStatement := `
+		UPDATE posts SET category_id = ?, title = ?, desc = ? WHERE id = ?;
+	`
+
+	result, err := p.db.Exec(sqlStatement, categoryID, title, description, postID)
+
+	if err != nil {
+		return err
+	}
+
+	count, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		return ErrPostNotFound
+	}
+
+	return nil
+}
+
+func (p *PostRepository) DeletePostByID(postID int) error {
+	sqlStatement := `DELETE FROM posts WHERE id = ?;`
+
+	result, err := p.db.Exec(sqlStatement, postID)
+
+	if err != nil {
+		return err
+	}
+
+	count, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		return ErrPostNotFound
+	}
+
+	return nil
 }
