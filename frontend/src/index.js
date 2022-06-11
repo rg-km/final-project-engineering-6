b51bf84e75852a6e4e63270f7c6648b11da71301
ArfanDwiSukmajaya <arfandwisukmajaya@MacBook-Air-Arfan.local>
2022-06-12 06:50:26 +0700

    feat:merging branch navbar-fe to branch main

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
->>>>>>> navbar-fe
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
