671251ef44f45a2a3983712269ae960d10d71cd8
Wilbert Chen <wilbertchen2001@gmail.com>
2022-06-11 17:59:56 +0700

    style comment card and add responsive to pages

--- frontend/src/components/PostCard/PostCard.jsx
+++ frontend/src/components/PostCard/PostCard.jsx
@@ -6,6 +6,7 @@ const PostCard = ({ page }) => {
   return (
     <div
       className={page === 'forum' ? 'post-card forum-post-card' : 'post-card'}
+      style={{ backgroundColor: page === 'comment' && '#f4f4f4' }}
     >
       <PostContent page={page} />
     </div>
