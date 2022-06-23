5e0c6248cbbaa74662bccdeab37ace6a4a645f5e
Wilbert Chen <wilbertchen2001@gmail.com>
2022-06-23 13:09:33 +0700

    feat: add expiry in local storage token

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,10 +1,34 @@
 import create from 'zustand';
 
+function getWithExpiry() {
+  const itemStr = localStorage.getItem('token');
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+
+  if (!itemStr) {
+    return null;
+  }
+
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem('token');
+    return null;
+  }
+
+  return item.token;
+}
+
 const useTokenStore = create((set) => ({
-  token: null || localStorage.getItem('token'),
+  token: getWithExpiry(),
   setToken: (input) => {
+    const now = new Date();
     set(() => ({ token: input }));
-    localStorage.setItem('token', input);
+    localStorage.setItem(
+      'token',
+      JSON.stringify({
+        token: input,
+        expiry: now.getTime() + 3600000,
+      })
+    );
   },
 }));
 
