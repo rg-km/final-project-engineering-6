2acce7bb865273e462b63198e54ce5e127ba9cdc
Yoga Cahya R <yogacahya08@gmail.com>
2022-07-04 22:59:32 +0700

    refactor: change homepage text

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -15,17 +15,17 @@ const HomePage = () => {
       <div className="container">
         <div className="words">
           <div className="title">
-            <h1>Don’t wanna Make you have A bad day</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequuntur odit quam sint sapiente aspernatur omnis accusamus rerum asperiores ipsam.</p>
+            <h1>Go enhance your school and college life</h1>
+            <p>Join other students around Indonesia. Do you want to ask something about school and college things? Just post it and other users will answer it.</p>
             <div className="btn">
-              <Link to="">
-                <Button variant="login">Join Forum</Button>
+              <Link to="/forum">
+                <Button variant="login">Open Forum</Button>
               </Link>
             </div>
           </div>
         </div>
         <div className="banner">
-          <img width={700} src={Banner} alt="" />
+          <img src={Banner} alt="" />
         </div>
       </div>
       <div className="what">
@@ -33,30 +33,34 @@ const HomePage = () => {
           <img src={Basis} alt="" />
         </div>
         <div className="explain">
-          <h2>Get quality, hyper-focused, uninterrupted work done </h2>
+          <h2>Go post forum or place your survey link </h2>
           <p>
-            Endless to-do lists and constant distractions from coworkers, calendars and cuddly pets means that you’re probably wasting a lot of time. Seque helps you to plan, focus and optimize your time so that you can get more done in less time.
+          We try to create a forum platform that students around Indonesia can use. They can ask about anything, share about anything, and expand their connections with other students.  
           </p>
           <br />
-          <p>Work distraction-free immersed in deep focus blocks, one task at a time. Increase your productivity by knowing exactly how and when you get the most done.</p>
+          <p>We also provide a place that can be used for sharing survey links. By placing your survey links in BASIS, another user can fill your survey links and you don't need to find other respondents.</p>
+          <br />
+          <p>we plan to expand our features so we can provide many services that can make your school or college life easier. So stay tuned.</p>
         </div>
       </div>
       <div className="fitur">
-        <div className="card">
-          <img src={Post} alt="" />
-          <h3>Post</h3>
+        <div className="text-fitur">
+          <h1>Our Service</h1>
+          <p>We make it easy for users to use our platform</p>
         </div>
-        <div className="card">
-          <img src={Survey} alt="" />
-          <h3>Survey</h3>
+        <div className="fitur-wrapper">
+          <div className="card">
+            <img src={Post} alt="" />
+            <h3>Post</h3>
+          </div>
+          <div className="card">
+            <img src={Survey} alt="" />
+            <h3>Survey</h3>
+          </div>
         </div>
-        <div className="card">
-          <img src={Event} alt="" />
-          <h3>Event</h3>
+        <div className="footer">
+          <p>copyright 2022 | team Basis</p>
         </div>
-      </div>
-      <div className="footer">
-        <p>Copyright 2022 | team Basis</p>
       </div>
     </div>
   );
